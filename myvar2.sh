2126e04bb5650ee8ca9216160e19bc662a1a4969
Jonathan Arouca <jonathanarouca@gmail.com>
2018-07-01 14:30:06 -0400

    add for, while loops

--- myvar2.sh
+++ myvar2.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
-echo "MYVAR is: $MYVAR"
-MYVAR="hi there"
-echo "MYVAR is: $MYVAR"
-echo "What's your name?"
-read USER_NAME
-echo "Hello $USER_NAME, I will create a file called ${USER_NAME}_file"
+# echo "MYVAR is: $MYVAR"
+# MYVAR="hi there"
+# echo "MYVAR is: $MYVAR"
+# echo "What's your name?"
+# read USER_NAME
+# echo "Hello $USER_NAME, I will create a file called ${USER_NAME}_file"
 # touch ${USER_NAME}_file
+
+# for loops
+
+# for i in 1 2 3 4 5
+# do
+#   echo "Looping .. number $i"
+# done
+#
+# for i in hello 1 * 2 goodbye
+# do
+#   echo "Looping ... i is set $i"
+# done
+
+# while loops
+
+INPUT_STRING=Hello
+while [ "$INPUT_STRING" != "bye" ]
+do
+  echo "Please type something in (bye to quit)"
+  read INPUT_STRING
+  echo "You typed: $INPUT_STRING"
+done
+
+while :
+do
+  echo "Please type something in (contol + c to quit)"
+  read INPUT_STRING
+  echo "You typed: $INPUT_STRING"
+done
