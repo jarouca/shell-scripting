c38dc8f1565a5ef5b4b87f21f9da14ca41d22623
Jonathan Arouca <jonathanarouca@gmail.com>
2018-09-24 22:47:22 -0400

    update backup shell script

--- database_backup.sh
+++ database_backup.sh
@@ -8,26 +8,26 @@ month=$( echo $(date) | cut -c '5-7' )
 year=$( echo $(date) | rev | cut -c '1-4' | rev )
 
 # if permanentStorage directory does not have a directory for the current year, make one
-$( ssh -p 5951 69.166.8.159 "[ -d /home/acordex/fmBackup/permanentStorage/$year ] || mkdir /home/acordex/fmBackup/permanentStorage/$year" )
+$( ssh -p 1234 11.111.1.111 "[ -d /remote/machine/backup/directory/permanentStorage/$year ] || mkdir /remote/machine/backup/directory/permanentStorage/$year" )
 
 # if permanent storage directory does not have a backup for this month, sync one to it,
 # otherwise add any newly created files without updating any already existing
-rsync -avzh -e "ssh -p 5951" --exclude '*.fp7' --ignore-existing /Library/FileMaker\ Server/Data/Databases/AcordexDB 69.166.8.159:/home/acordex/fmBackup/permanentStorage/$year/$month/
+rsync -avzh -e "ssh -p 1234" --ignore-existing /local/database/directory 11.111.1.111:/remote/machine/backup/directory/permanentStorage/$year/$month
 
 # if rsync was not successful, send email alert
 if [ "$?" != "0" ]
 then
-  echo "Remote Filemaker permanent storage database backup failed. A Cron job is scheduled to run the backup script Coral_FM/Coral\ HD/Users/acordex/Desktop/Scripts/fmRemoteBackup.sh daily." | mail -s Filemaker remote permanent storage backup failed - faxalert@acordex.com
+  echo "Remote permanent storage database backup failed." | mail -s Remote permanent storage backup failed - admin@db.com
 fi
 
 # sync a new daily backup to the rolling storage
-rsync -avzh -e "ssh -p 5951" --exclude '*.fp7' /Library/FileMaker\ Server/Data/Databases/AcordexDB 69.166.8.159:/home/acordex/fmBackup/rollingStorage/$filename/
+rsync -avzh -e "ssh -p 1234" /local/database/directory 11.111.1.111:/remote/machine/backup/directory/rollingStorage/$filename/
 
 # if rsync was not successful, send email alert
 if [ "$?" != "0" ]
 then
-  echo "Remote Filemaker rolling storage database backup failed. A Cron job is scheduled to run the backup scriptCoral_FM/Coral\ HD/Users/acordex/Desktop/Scripts/fmRemoteBackup.sh daily." | mail -s Filemaker remote rolling storage backup failed - faxalert@acordex.com
+  echo "Remote rolling storage database backup failed." | mail -s Remote rolling storage backup failed - admin@db.com
 fi
 
 # delete rollingStorage backups older than 62 days
-$( ssh -p 5951 69.166.8.159 "find /home/acordex/fmBackup/rollingStorage/* -prune -mtime +62 -exec rm -rf {} \;" )
+$( ssh -p 1234 11.111.1.111 "find /remote/machine/backup/directory/rollingStorage/* -prune -mtime +62 -exec rm -rf {} \;" )
